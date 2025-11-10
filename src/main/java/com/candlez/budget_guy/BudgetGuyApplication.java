fd0c509461cd6af395a0ec46636777d90c3ad895
candlez <thomasmcnamara@vt.edu>
2025-11-09 21:31:06 -0500

    init

---
+++ src/main/java/com/candlez/budget_guy/BudgetGuyApplication.java
@@ -0,0 +1,13 @@
+package com.candlez.budget_guy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetGuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetGuyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/candlez/budget_guy/BudgetGuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.candlez.budget_guy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetGuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
