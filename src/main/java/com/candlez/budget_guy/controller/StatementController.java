d7c294203ebe65804d8d32d3d7dc9a5c8c0c43bb
candlez <thomasmcnamara@vt.edu>
2026-01-01 22:03:55 -0500

    feat: [MVP-3] manually serve api from /api

--- src/main/java/com/candlez/budget_guy/controller/StatementController.java
+++ src/main/java/com/candlez/budget_guy/controller/StatementController.java
@@ -18,7 +18,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.time.LocalDate;
 
 @RestController
-@RequestMapping("/statement")
+@RequestMapping("/api/statement")
 public class StatementController {
 
     @Autowired
