e26f4dd0af7a587454183be52e64a8355088f861
candlez <thomasmcnamara@vt.edu>
2026-01-14 22:07:47 -0500

    feat: [MVP-3] forward errors to SPA

--- src/main/java/com/candlez/budget_guy/controller/AngularForwardController.java
+++ src/main/java/com/candlez/budget_guy/controller/AngularForwardController.java
@@ -1,7 +1,13 @@
 package com.candlez.budget_guy.controller;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * the purpose of this controller is to direct all traffic that does not start with "/api" and is not a
@@ -9,10 +15,22 @@ import org.springframework.web.bind.annotation.GetMapping;
  * one is not found, it will fall back to the wildcard route that has been configured
  */
 @Controller
-public class AngularForwardController {
+public class AngularForwardController implements ErrorController {
 
     @GetMapping("/{path:^(?!api)(?!.*\\.).*$}")
     public String forward() {
+        return "forward:/index.html";
+    }
+
+    @RequestMapping(
+            value = "${server.error.path:/error}",
+            produces = MediaType.TEXT_HTML_VALUE
+    )
+    public String forwardError(HttpServletRequest request, HttpServletResponse response) {
+        if (request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE) instanceof Integer status) {
+            response.setStatus(status);
+        }
+
         return "forward:/index.html";
     }
 }
