cad4200cbfbef1d2b57c958bc7081e08fc63fe9a
candlez <thomasmcnamara@vt.edu>
2026-01-17 21:27:08 -0500

    feat: [MVP-3] add stub error and login components

--- src/main/frontend/src/app/app.routes.ts
+++ src/main/frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ErrorComponent } from './components/error/error.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "error", component: ErrorComponent},
+  {path: "login", component: LoginComponent}
+];
