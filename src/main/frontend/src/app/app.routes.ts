c685859b0f1ebfc26da6168bbc6701649248d7f4
candlez <thomasmcnamara@vt.edu>
2026-01-19 23:16:20 -0500

    feat: [MVP-3] generate not found skeleton

--- src/main/frontend/src/app/app.routes.ts
+++ src/main/frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ErrorComponent } from './components/error/error.component';
 import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
   {path: "error", component: ErrorComponent},
-  {path: "login", component: LoginComponent}
+  {path: "login", component: LoginComponent},
+  {path: "**", component: NotFoundComponent}
 ];
