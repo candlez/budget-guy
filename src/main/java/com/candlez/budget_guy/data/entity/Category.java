66e82990040ab1ef071825736b076556eb41b48b
candlez <thomasmcnamara@vt.edu>
2026-01-27 21:17:57 -0500

    fix: [MVP-3] finish Category entity

--- src/main/java/com/candlez/budget_guy/data/entity/Category.java
+++ src/main/java/com/candlez/budget_guy/data/entity/Category.java
@@ -1,4 +1,71 @@
 package com.candlez.budget_guy.data.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "categories")
 public class Category {
+
+    @Id
+    @Column(name = "id", columnDefinition = "BINARY(16)")
+    private UUID categoryID;
+
+    @Column(name = "user_id", columnDefinition = "BINARY(16)")
+    private UUID userID;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+    // getters and setters
+    public UUID getCategoryID() {
+        return this.categoryID;
+    }
+
+    public void setCategoryID(UUID statementID) {
+        this.categoryID = statementID;
+    }
+
+    public UUID getUserID() {
+        return this.userID;
+    }
+
+    public void setUserID(UUID userID) {
+        this.userID = userID;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Instant getCreatedAt() {
+        return this.createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
 }
