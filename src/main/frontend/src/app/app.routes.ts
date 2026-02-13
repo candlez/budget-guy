3873fe39e34faa1ddf08b8c2c49933296b030a0e
candlez <thomasmcnamara@vt.edu>
2026-02-12 19:00:31 -0500

    feat: [MVP-4] remove error component

--- src/main/frontend/src/app/app.routes.ts
+++ src/main/frontend/src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
-import { ErrorComponent } from './components/error/error.component';
 import { LoginComponent } from './components/login/login.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
-  {path: "error", component: ErrorComponent},
   {path: "login", component: LoginComponent},
   {path: "**", component: NotFoundComponent}
 ];
