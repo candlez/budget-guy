4008953c13bb3e594045a82b722f29dfe6388cc4
candlez <thomasmcnamara@vt.edu>
2026-02-26 22:02:27 -0500

    feat: [MVP-4] add wiring for snackbar

--- src/main/frontend/src/app/components/create-categories-page/create-categories-page.component.ts
+++ src/main/frontend/src/app/components/create-categories-page/create-categories-page.component.ts
@@ -5,6 +5,8 @@ import { MatFormField, MatInput, MatLabel } from '@angular/material/input';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CategoryService } from '../../services/category.service';
 import { CategoryRequestDto } from '../../model/dto/CategoryRequest.dto';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Category } from '../../model/domain/Category';
 
 @Component({
   selector: 'app-create-categories-page',
@@ -23,7 +25,11 @@ import { CategoryRequestDto } from '../../model/dto/CategoryRequest.dto';
 export class CreateCategoriesPageComponent {
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private categoryService: CategoryService) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private snackBar: MatSnackBar,
+    private categoryService: CategoryService
+  ) {
     this.form = this.formBuilder.group({
       name: [''],
       description: ['']
@@ -31,11 +37,25 @@ export class CreateCategoriesPageComponent {
   }
 
   submitForm(): void {
-
     const dto: CategoryRequestDto = {
       name: this.form.value.name,
       description: this.form.value.description
     }
-    this.categoryService.createCategory(dto).subscribe(res => console.log(res));
+    this.categoryService.createCategory(dto).subscribe({
+      next: (value: Category): void => {
+        this.successSnackBar();
+      },
+      error: (err: Error): void => {
+        this.errorSnackBar(err.message);
+      }
+    });
+  }
+
+  successSnackBar(): void {
+    this.snackBar.open("Created Category", "dismiss",  { duration: 1500, horizontalPosition: "end" });
+  }
+
+  errorSnackBar(message: string): void {
+    this.snackBar.open(message, "dismiss",  { duration: 1500, horizontalPosition: "end" });
   }
 }
