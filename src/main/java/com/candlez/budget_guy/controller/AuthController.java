a7af2d8c602454127b8dc99a97f596ca69b962c8
candlez <thomasmcnamara@vt.edu>
2026-03-12 22:00:45 -0400

    fix: [MVP-4] improve exception handling in auth controller

--- src/main/java/com/candlez/budget_guy/controller/AuthController.java
+++ src/main/java/com/candlez/budget_guy/controller/AuthController.java
@@ -17,7 +17,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -52,36 +51,25 @@ public class AuthController {
     @PostMapping("/signup")
     public ResponseEntity<?> signup(@RequestBody @Valid SignupRequestDto signupRequestDto) {
 
-        UserResponseDto userResponseDto;
-        try {
-            User user = authService.signup(signupRequestDto);
-            userResponseDto = this.userMapper.toResponseDto(user);
-        } catch (Exception e) {
-            return ApiErrorResponse.sendOne(HttpStatus.INTERNAL_SERVER_ERROR, "Something went wrong unexpectedly");
-        }
+        User user = authService.signup(signupRequestDto);
+        UserResponseDto userResponseDto = this.userMapper.toResponseDto(user);
+
         return ApiResponse.sendCreated(userResponseDto.getUserId(), userResponseDto);
     }
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody @Valid LoginRequestDto loginRequestDto, HttpServletResponse response) {
 
-        UserResponseDto userResponseDto;
-        try {
-            User user = authService.login(loginRequestDto);
+        User user = authService.login(loginRequestDto);
 
-            String token = authService.generateToken(user);
+        String token = authService.generateToken(user);
 
-            ResponseCookie cookie = authService.createCookie(token);
+        ResponseCookie cookie = authService.createCookie(token);
 
-            response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
 
-            userResponseDto = this.userMapper.toResponseDto(user);
-        } catch (UnauthorizedException e) {
-            throw e;
-        } catch (Exception e) {
-            LOGGER.error("Something went wrong!", e);
-            return ApiErrorResponse.sendOne(HttpStatus.INTERNAL_SERVER_ERROR, "Something went wrong unexpectedly");
-        }
+        UserResponseDto userResponseDto = this.userMapper.toResponseDto(user);
+
         return ApiResponse.sendOne(userResponseDto.getUserId(), userResponseDto);
     }
 
